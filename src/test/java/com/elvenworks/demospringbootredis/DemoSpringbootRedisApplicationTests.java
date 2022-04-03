74d0ac10d44468af74f1c0518ee50494b1263299
brunopereiraelven <56091663+brunopereiraelven@users.noreply.github.com>
2022-04-02 23:22:53 -0300

    Initial commit

---
+++ src/test/java/com/elvenworks/demospringbootredis/DemoSpringbootRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elvenworks.demospringbootredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringbootRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
